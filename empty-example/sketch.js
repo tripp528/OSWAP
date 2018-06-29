435a40134d07ee8f82d1f4527fe792c36b33db83
tripp528 <tripp528@gmail.com>
2018-06-29 10:47:37 -0600

    added js file, created drawing script (black only)

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  // put setup code here
+	createCanvas(400,400);
+
+	background(220);
 }
 
 function draw() {
-  // put drawing code here
+
+	if (mouseIsPressed) {
+		fill(0);
+		ellipse(mouseX, mouseY, 20, 20);
+	} else {
+		
+	}
 }
